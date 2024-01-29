808983223aa5fe200d05ad6d3341b1df1e5cfcb0
RalucaM13 <157896258+RalucaM13@users.noreply.github.com>
2024-01-29 14:09:20 +0000

    icon

--- src/functions.js
+++ src/functions.js
@@ -62,9 +62,9 @@ function displayForecast(response) {
       forecastHtml +
       `<div class="weather-forecast" id="forecast">
               <div class="weather-forecast-date">Tuesday</div>
-              <div  class= "weather-forecast-icon"><img src="${
+              <div  ><img src="${
                 day.condition.icon_url
-              }"/></div>
+              }" class= "weather-forecast-icon"/></div>
               <div class="weather-forecast-temperatures">
                 <span class="weather-forecast-temperature-max">${Math.round(
                   day.temperature.maximum
